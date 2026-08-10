1e5228aa45707835d8e3cd41a069f23479eb2e5f
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-10 13:38:37 +0700

    feat: tambah controller untuk update data genre

--- controller/genreController.js
+++ controller/genreController.js
@@ -19,3 +19,18 @@ async function createGenre(req, res) {
         res.status(500).json({ error: err.message });
     }
 }
+
+async function updateGenre(req, res) {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const genre = await Genre.findByPk(id);
+        if (!genre) return res.status(404).json({ error: "Genre tidak ditemukan" });
+
+        genre.name = name;
+        await genre.save();
+        res.status(200).json(genre);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
