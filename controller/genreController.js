79dbc06f2526883c02340eef45eda3e76719747c
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-10 13:37:50 +0700

    feat: tambah controller untuk menambahkan data genre

--- controller/genreController.js
+++ controller/genreController.js
@@ -9,3 +9,13 @@ async function getAllGenre(req, res) {
         res.status(500).json({ error: err.message });
     }
 }
+
+async function createGenre(req, res) {
+    try {
+        const { name } = req.body;
+        const newGenre = await Genre.create({ name });
+        res.status(201).json(newGenre);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
