b5358f7b2e9a60d7e11c3905c663231d447d8115
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-10 13:41:37 +0700

    feat: tambah controller untuk menampilkan detail komik berdasarkan id

--- controller/komikController.js
+++ controller/komikController.js
@@ -12,3 +12,18 @@ async function getAllKomik(req, res) {
         res.status(500).json({ error: "failed to fetch komik" });
     }
 }
+
+
+async function getKomikById(req, res) {
+    const { id } = req.params;
+    try {
+        const komik = await KomikModel.findByPk(id);
+        if (!komik) {
+            return res.status(404).json({ error: "komik not found" });
+        }
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error("Error fetching komik by ID:", err.message);
+        res.status(500).json({ error: "failed to fetch komik" });
+    }
+}
