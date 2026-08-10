f391cea8d3e53d4aeb4a9b5026617cc395d4c590
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2026-08-10 13:52:04 +0700

    feat: tambah routing untuk registrasi dan login pengguna

--- routes/api.js
+++ routes/api.js
@@ -4,3 +4,7 @@ const komikController = require("../controller/komikController");
 const userController = require("../controller/userController");
 const genreController = require("../controller/genreController");
 const authMiddleware = require("../middleware/authMiddleware");
+
+// Auth Routes
+router.post("/register", userController.register);
+router.post("/login", userController.login);
